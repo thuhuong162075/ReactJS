46d06225cdfe5d85f91fd887d24dd3e046f58f1f
Thu Huong <huong.ttt171298@gmail.com>
2020-07-31 14:25:43 +0700

    api search

--- my-movie/src/components/SearchMovies.js
+++ my-movie/src/components/SearchMovies.js
@@ -33,7 +33,7 @@ function SearchMovies(props) {
   const onClickSearch = () => {
     dispatch(onShowSearch(true))
     if(filter !== '') {
-        callApi(`${'https://api.themoviedb.org/3/discover/movie?api_key=0aecc06bb4fadb06b5f071fef0c2ce6d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page='}${_page}`,
+        callApi(`${'https://api.themoviedb.org/3/search/movie?query='}${filter}${'&api_key=cfe422613b250f702980a3bbf9e90716&page='}${_page}`,
         'GET',null).then(res=> {
           dispatch(actPaginationSearch({
             page: res.data.page,
